b963b29a0a89d0425a98d4c019bb1a609e2bdc73
ejaysrngy <ejaysarangay@gmail.com>
2022-01-22 22:53:53 +0800

    remove setState fxn

--- src/components/IndividualItem.js
+++ src/components/IndividualItem.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line jsx-a11y/alt-text
+import { Link } from "react-router-dom";
 import React from "react";
 import "./IndividualItem.css";
 
@@ -8,17 +8,50 @@ export default function IndividualItem({
   itemPrice,
   itemImg,
 }) {
+  // const [chosenItem, setChosenItem] = React.useState({
+  //   chosenName: null,
+  //   chosenColor: null,
+  //   chosenPrice: null,
+  //   chosenImg: null,
+  // });
+
+  // function onClickHandler() {
+  //   setChosenItem((prevState) => {
+  //     return {
+  //       ...prevState,
+  //       chosenColor: itemColor,
+  //       chosenName: itemName,
+  //       chosenPrice: itemPrice,
+  //       chosenImg: itemImg,
+  //     };
+  //   });
+  // }
+
   return (
     <div className="item-container">
-      <img src={itemImg} alt={itemName}/>
+      <img src={itemImg} alt={itemName} />
       <div className="item-container__desc col-lg">
         <h6> {itemName} </h6>
         <h5>
-          Php {itemPrice.toLocaleString(undefined, { minimumFractionDigits: 2 })}
+          Php{" "}
+          {itemPrice.toLocaleString(undefined, { minimumFractionDigits: 2 })}
         </h5>
+        <h6>{itemColor}</h6>
       </div>
       <div className="item-container__buyNow">
-          <button> Buy Now </button>
+        <Link
+          to={{
+            pathname: "/checkout",
+            state: {
+              chosenColor: itemColor,
+              chosenName: itemName,
+              chosenPrice: itemPrice,
+              chosenImg: itemImg,
+            },
+          }}
+        >
+          <button>Buy Now</button>
+        </Link>
       </div>
     </div>
   );
