61cc640f68394ce7570c86d49a2f88b9ea1a5207
ejaysrngy <ejaysarangay@gmail.com>
2022-01-22 20:53:53 +0800

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Shop from "./components/Shop";
+import Footer from "./components/Footer";
 import "./components/App.css"
 
 export default function App() {
