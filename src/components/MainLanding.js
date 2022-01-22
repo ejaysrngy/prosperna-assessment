cb334e02845acce9bfacee2693b5e6298ebf22b4
ejaysrngy <ejaysarangay@gmail.com>
2022-01-22 21:56:27 +0800

    create MainLanding page component

---
+++ src/components/MainLanding.js
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Content from "./Content";
+import Shop from "./Shop";
+import Footer from "./Footer";
+import React from "react";
+
+export default function MainLanding() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Shop />
+      <Footer />
+    </div>
+  );
+}
