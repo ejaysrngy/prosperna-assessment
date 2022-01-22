119811b73457262813058a2e5044bab983cdefac
ejaysrngy <ejaysarangay@gmail.com>
2022-01-22 17:23:50 +0800

    initialized files

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css"
+
+
+export default function Header() {
+    return (
+        <header>
+            <img className="logo" src="./assets/sneakerfreak.png" alt="logo" />
+            <nav>
+                <ul className="nav-list">
+                    <li className="nav-list__item"> <a href="index.html"> Home </a></li>
+                    <li className="nav-list__item"> <a href="#products"> Shop </a> </li>
+                </ul>
+            </nav>
+            <a className="contact" href="#contact"> <button> Contact </button></a>
+        </header>
+    )
+}
