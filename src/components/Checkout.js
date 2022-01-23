c828f11e8fdaf8f8724fa618613e9c2f2d6871ea
ejaysrngy <ejaysarangay@gmail.com>
2022-01-23 19:00:06 +0800

    add Total; c styles

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -10,13 +10,13 @@ export default function Checkout() {
   const { chosenName, chosenColor, chosenPrice, chosenImg } = chosenItem;
   const [paypalPayment, setPaypalPayment] = React.useState(false);
 
-  let checkoutPrice
-  if (cart.length === 0){
+  let checkoutPrice;
+  if (cart.length === 0) {
     checkoutPrice = chosenPrice;
   } else {
     checkoutPrice = cart.reduce((total, curr) => {
-      return total + curr.chosenPrice 
-    }, 0)
+      return total + curr.chosenPrice;
+    }, 0);
   }
 
   return (
@@ -29,43 +29,44 @@ export default function Checkout() {
           <div className="shipping-container__details">
             <form>
               <label> First Name </label>
-              <input type="text"/>
+              <input type="text" />
               <label> Last Name </label>
-              <input type="text"/>
+              <input type="text" />
               <label> Email </label>
-              <input type="email"/>
+              <input type="email" />
               <label> Address Line 1 </label>
-              <input type="text"/>
+              <input type="text" />
               <label> Address Line 2 </label>
-              <input type="text"/>
+              <input type="text" />
               <label> City </label>
-              <input type="text"/>
+              <input type="text" />
               <label> Region/State </label>
-              <input type="text"/>
+              <input type="text" />
               <label> Postal Code </label>
               <input type="number" />
             </form>
             <div className="shipping-container__button">
               {paypalPayment ? (
-                <PayPalButtons createOrder={(data, actions) => {
-                return actions.order.create({
-                    purchase_units: [
+                <PayPalButtons
+                  createOrder={(data, actions) => {
+                    return actions.order.create({
+                      purchase_units: [
                         {
-                            description: chosenName,
-                            amount: {
-                                value: checkoutPrice,
-                            }
-                        }
-                    ]
-                });
-            }}
-            onApprove={(data, actions) => {
-                return actions.order.capture().then((details) => {
-                    const name = details.payer.name.given_name;
-                    alert(`Transaction completed by ${name}`);
-                });
-            }}
-            />
+                          description: chosenName,
+                          amount: {
+                            value: checkoutPrice,
+                          },
+                        },
+                      ],
+                    });
+                  }}
+                  onApprove={(data, actions) => {
+                    return actions.order.capture().then((details) => {
+                      const name = details.payer.name.given_name;
+                      alert(`Transaction completed by ${name}`);
+                    });
+                  }}
+                />
               ) : (
                 <button
                   onClick={() => {
@@ -83,43 +84,58 @@ export default function Checkout() {
         </div>
         <div className="summary-container col-lg-4 col-md-6">
           <h1> Summary </h1>
-          {cart.length === 0 ? 
-          // If cart length is less than 0
-          <div className="summary-container__item row">
-            <div className="item-img col-4">
-              {chosenImg != null ? <img src={chosenImg} alt="shoe" /> : null}
+          <div className="total-container row">
+            <h5 className="col-6"> Total: </h5>
+            <h6 className="col-6">
+              {" "}
+              Php {checkoutPrice.toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+              })}{" "}
+            </h6>
+          </div>
+          {cart.length === 0 ? (
+            // If cart length is less than 0
+            <div className="summary-container__item row">
+              <div className="item-img col-4">
+                {chosenImg != null ? <img src={chosenImg} alt="shoe" /> : null}
+              </div>
+              <div className="item-desc col">
+                <h3> {chosenName} </h3>
+                <h6> {chosenColor} </h6>
+                {chosenPrice != null && (
+                  <p>
+                    Php{" "}
+                    {chosenPrice.toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                    })}{" "}
+                  </p>
+                )}
+              </div>
             </div>
-            <div className="item-desc col">
-              <h3> {chosenName} </h3>
-              <h6> {chosenColor} </h6>
-              {chosenPrice != null && (
-                <p>
-                  Php{" "}
-                  {chosenPrice.toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                  })}{" "}
-                </p>
-              )}
-            </div>
-            </div> : 
+          ) : (
             // if cart length is greater than 0
-            (cart.map((item, index) => (<div key={index} className="summary-container__item row">
-            <div className="item-img col-4">
-              {item.chosenImg != null ? <img src={item.chosenImg} alt="shoe" /> : null}
-            </div>
-            <div className="item-desc col">
-              <h3> {item.chosenName} </h3>
-              <h6> {item.chosenColor} </h6>
-              {item.chosenPrice != null && (
-                <p>
-                  Php{" "}
-                  {item.chosenPrice.toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                  })}{" "}
-                </p>
-              )}
-            </div>
-          </div>)))}
+            cart.map((item, index) => (
+              <div key={index} className="summary-container__item row">
+                <div className="item-img col-4">
+                  {item.chosenImg != null ? (
+                    <img src={item.chosenImg} alt="shoe" />
+                  ) : null}
+                </div>
+                <div className="item-desc col">
+                  <h3> {item.chosenName} </h3>
+                  <h6> {item.chosenColor} </h6>
+                  {item.chosenPrice != null && (
+                    <p>
+                      Php{" "}
+                      {item.chosenPrice.toLocaleString(undefined, {
+                        minimumFractionDigits: 2,
+                      })}{" "}
+                    </p>
+                  )}
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
 
