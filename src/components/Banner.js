cc5dea78b9f5a7bdf3514905f6cb93601abda6be
ejaysrngy <ejaysarangay@gmail.com>
2022-01-23 17:14:55 +0800

    add Banner comp; style banner

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Banner.css";
+
+export default function Banner(){
+    return (
+        <div className="banner-container">
+            <img src="/images/banner.jpg" alt="banner" />
+        </div>
+    )
+}
