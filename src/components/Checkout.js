15d8a3207bca9a4ea990665c9403839f0874b477
ejaysrngy <ejaysarangay@gmail.com>
2022-01-23 14:49:27 +0800

    add Paypal components, params

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,58 +1,78 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import React, { useContext } from "react";
+import React from "react";
 import { ItemContext } from "./ItemContext";
+import { PayPalButtons } from "@paypal/react-paypal-js";
 import "./Checkout.css";
 
 export default function Checkout() {
-  const { chosenItem, setChosenItem } = React.useContext(ItemContext);
+  const { chosenItem } = React.useContext(ItemContext);
   const { chosenName, chosenColor, chosenPrice, chosenImg } = chosenItem;
+  const [paypalPayment, setPaypalPayment] = React.useState(false);
+
   return (
     <div>
       <Header />
       <div className="checkout-page row">
-        <div className="shipping-container col-lg-8 col-md-12">
+        <h1> Checkout </h1>
+        <div className="shipping-container col-lg-8 col-md-12 row">
           <h1> Shipping Details </h1>
           <div className="shipping-container__details">
             <form>
-              <div>
-                <h6> First Name </h6>
-                <input />
-              </div>
-              <div>
-                <h6> Last Name </h6>
-                <input />
-              </div>
-              <div>
-                <h6> Address Line 1 </h6>
-                <input />
-              </div>
-              <div>
-                <h6> Address Line 2 </h6>
-                <input />
-              </div>
-              <div>
-                <h6> City </h6>
-                <input />
-              </div>
-              <div>
-                <h6> Region/State </h6>
-                <input />
-              </div>
-              <div>
-                <h6> Postal Code </h6>
-                <input />
-              </div>
-              <div>
-                <h6> First Name </h6>
-                <input />
-              </div>
+              <label> First Name </label>
+              <input />
+              <label> Last Name </label>
+              <input />
+              <label> Address Line 1 </label>
+              <input />
+              <label> Address Line 2 </label>
+              <input />
+              <label> City </label>
+              <input />
+              <label> Region/State </label>
+              <input />
+              <label> Postal Code </label>
+              <input />
             </form>
+            <div className="shipping-container__button">
+              {paypalPayment ? (
+                <PayPalButtons createOrder={(data, actions) => {
+                return actions.order.create({
+                    purchase_units: [
+                        {
+                            description: chosenName,
+                            amount: {
+                                value: chosenPrice,
+                            }
+                        }
+                    ]
+                });
+            }}
+            onApprove={(data, actions) => {
+                return actions.order.capture().then((details) => {
+                    const name = details.payer.name.given_name;
+                    alert(`Transaction completed by ${name}`);
+                });
+            }}
+            />
+              ) : (
+                <button
+                  onClick={() => {
+                    setPaypalPayment(true);
+                  }}
+                >
+                  {" "}
+                  Submit & Pay with Paypal{" "}
+                </button>
+              )}
+
+              <button> Submit & Pay COD </button>
+            </div>
           </div>
         </div>
-        <div className="checkout-container col-lg-4 col-md-6">
-          <h1> Checkout</h1>
-          <div className="checkout-container__item row">
+        <div className="summary-container col-lg-4 col-md-6">
+          <h1> Summary </h1>
+          <div className="summary-container__item row">
             <div className="item-img col-4">
               {chosenImg != null ? <img src={chosenImg} alt="shoe" /> : null}
             </div>
@@ -69,10 +89,6 @@ export default function Checkout() {
               )}
             </div>
             {/* ^^ item-desc */}
-            <div className="quantity col-lg-3">
-              <button className="decrease"> - </button>
-              <button className="increase"> + </button>
-            </div>
           </div>
           {/* ^^ checkout-container__item */}
         </div>
