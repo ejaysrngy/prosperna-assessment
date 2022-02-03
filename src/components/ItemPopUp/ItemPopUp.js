d144b1ba61d36a454724a48074132044da64e9bf
ejaysrngy <ejaysarangay@gmail.com>
2022-02-03 19:00:51 +0800

    add ItemPopUp code

--- src/components/ItemPopUp/ItemPopUp.js
+++ src/components/ItemPopUp/ItemPopUp.js
@@ -1,9 +1,29 @@
 import React from "react";
+import ITEMS from "../../assets/items.json"
+import "./ItemPopUp.css"
 
 export default function ItemPopUp(){
+    const SHOE_SIZES = [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5]
+
+    const {products} = ITEMS
     return (
-        <React.Fragment>
-            
-        </React.Fragment>
+        <div className="item-popup">
+            <div className="item-popup__indivitem row">
+                <div className="item-popup__image col-6">
+                    <img src={products[0].img} alt="item-img" />
+                </div>
+                <div className="item-popup__desc col-4">
+                    <h1> {products[0].name} </h1>
+                    <h4> Php {products[0].price.toLocaleString(undefined, { minimumFractionDigits: 2 })} </h4> 
+                    <div className="item-popup__sizes">
+                        {SHOE_SIZES.map((currItem, index) => {
+                            return (
+                                <button className="shoesize-button"> {currItem} </button>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
