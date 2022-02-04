ef1de9d242ed923cd3ffeb38159a96810ee13ada
ejaysrngy <ejaysarangay@gmail.com>
2022-02-04 16:19:48 +0800

    add quantity state; button

--- src/components/ItemPopUp/ItemPopUp.js
+++ src/components/ItemPopUp/ItemPopUp.js
@@ -1,29 +1,48 @@
 import React from "react";
-import ITEMS from "../../assets/items.json"
-import "./ItemPopUp.css"
+import ITEMS from "../../assets/items.json";
+import "./ItemPopUp.css";
 
-export default function ItemPopUp(){
-    const SHOE_SIZES = [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5]
+export default function ItemPopUp() {
+  const SHOE_SIZES = [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5];
+  const [quantity, setQuantity] = React.useState(0);
 
-    const {products} = ITEMS
-    return (
-        <div className="item-popup">
-            <div className="item-popup__indivitem container-flex row">
-                <div className="item-popup__image col-6">
-                    <img src={products[0].img} alt="item-img" />
-                </div>
-                <div className="item-popup__desc col-4">
-                    <h1> {products[0].name} </h1>
-                    <h4> Php {products[0].price.toLocaleString(undefined, { minimumFractionDigits: 2 })} </h4> 
-                    <div className="item-popup__sizes">
-                        {SHOE_SIZES.map((currItem, index) => {
-                            return (
-                                <button key={index} className="shoesize-button"> {currItem} </button>
-                            )
-                        })}
-                    </div>
-                </div>
-            </div>
+
+  const { products } = ITEMS;
+  return (
+    <div className="item-popup">
+      <div className="item-popup__indivitem container-flex row">
+        <div className="item-popup__image col-6">
+          <img src={products[0].img} alt="item-img" />
         </div>
-    )
+        <div className="item-popup__desc col-4">
+          <h1> {products[0].name} </h1>
+          <h4>
+            {" "}
+            Php{" "}
+            {products[0].price.toLocaleString(undefined, {
+              minimumFractionDigits: 2,
+            })}{" "}
+          </h4>
+          <div className="item-popup__sizes">
+            {SHOE_SIZES.map((currItem, index) => {
+              return (
+                <button
+                  key={index}
+                  className="shoesize-button"
+                >
+
+                  {currItem}
+                </button>
+              );
+            })}
+          </div>
+          <div className="item-popup__quantity">
+            <button onClick={() => setQuantity(quantity-1)}> - </button>
+            <div className="quantity">{quantity} </div>
+            <button onClick={() => setQuantity(quantity+1)}> + </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
