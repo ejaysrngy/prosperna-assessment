2f1f1c29f1a578cd0bfdb6c9d5bdd65b0a310f9b
ejaysrngy <ejaysarangay@gmail.com>
2022-02-06 14:09:07 +0800

    add item context; add ternary for ItemPopUp

--- src/components/MainLanding.js
+++ src/components/MainLanding.js
@@ -4,16 +4,19 @@ import Shop from "../components/Shop/Shop";
 // import Footer from "../components/Footer/Footer";
 import Banner from "../components/Banner/Banner";
 import ItemPopUp from "./ItemPopUp/ItemPopUp";
+import { ItemContext } from "./ItemContext";
 import React from "react";
 
 export default function MainLanding() {
+  const {cart} = React.useContext(ItemContext);
+
 
   return (
     <div>
         {/* <Header /> */}
         <Content />
         <Shop />
-        <ItemPopUp />
+        {cart.length > 0 && <ItemPopUp />}
         <Banner />
         {/* <Footer /> */}
     </div>
