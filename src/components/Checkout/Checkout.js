924e841d6cda4f9a761154c9df70aedab28b71ba
ejaysrngy <ejaysarangay@gmail.com>
2022-02-09 19:31:46 +0800

    remove header/footer comments

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,5 +1,4 @@
-// import Header from "../Header/Header";
-// import Footer from "../Footer/Footer";
+
 import React from "react";
 import { ItemContext } from "../ItemContext";
 import { PayPalButtons } from "@paypal/react-paypal-js";
